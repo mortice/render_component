ec0174d023d58add1193d29866e05990e1def079
Ed Wildgoose <git@wildgooses.com>
2009-05-23 22:05:30 +0100

    Get most of the rests to pass - fairly trivial and obvious changes
    Inspiration from: http://github.com/weplay/render_component/commit/236c1ae2d089afbf6bc6bf78a6a515e80f12c00f

--- lib/components.rb
+++ lib/components.rb
@@ -50,7 +50,7 @@ module Components
       # Renders the component specified as the response for the current method
       def render_component(options) #:doc:
         component_logging(options) do
-          render_for_text(component_response(options, true).body, response.headers["Status"])
+          render_for_text(component_response(options, true).body, response.status)
         end
       end
 
